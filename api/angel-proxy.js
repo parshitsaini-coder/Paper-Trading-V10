781509b7df272967796655db56a612d5cf98d05c
parshitsaini-coder <parshitsaini@gmail.com>
2026-02-25 11:13:20 +0530

    Update angel-proxy.js

--- api/angel-proxy.js
+++ api/angel-proxy.js
@@ -61,7 +61,7 @@ async function getAngelToken() {
   return cachedToken;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
